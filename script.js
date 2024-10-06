844b64518a183d251dad486411a4a2279c4d3932
Navneet_thakur <127828104+navneet451@users.noreply.github.com>
2024-10-06 18:03:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+  // Check if it's a leap year
+  if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    return 366;
+  } else {
+    return 365;
+  }
+}
